1f8c9f7f8f9e2efe110927129787e24083f906ea
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2018-02-12 19:54:32 -0200

    Comitando cache

--- app/assets/script/cache.js
+++ app/assets/script/cache.js
@@ -1,6 +1,6 @@
 var DEV = location.host == 'localhost:9001';
 
-if ('serviceWorker' in navigator && DEV) {
+if ('serviceWorker' in navigator && !DEV) {
     navigator.serviceWorker.register('app/assets/script/sw.js').then(function (registration) {
         console.log('Service Worker Registered.');
 
